a8534fa5523cf69cf81bc6ae9ea7acb105cc5321
KevinMokoro <klosvoke1@gmail.com>
2021-04-12 14:47:42 +0300

    Constructed a constructor in CaesarCipher.java,added a set input  attribute and its getter method.

--- src/main/java/models/CaesarCipher.java
+++ src/main/java/models/CaesarCipher.java
@@ -1,8 +1,15 @@
 package models;
 
 public class CaesarCipher{
+    private String input;
 
     public CaesarCipher(String input, String output) {
+        this.input = input;
 
+    }
+
+
+    public String getInput() {
+        return this.input;
     }
 }
--- src/test/java/models/CaesarCipherTest.java
+++ src/test/java/models/CaesarCipherTest.java
@@ -2,6 +2,8 @@ package models;
 
 import org.junit.*;
 
+import java.util.concurrent.Callable;
+
 import static org.junit.Assert.*;
 
 public class CaesarCipherTest {
@@ -9,5 +11,18 @@ public class CaesarCipherTest {
     public void newCaesarCipher_instantiatesCorrectly() {
         CaesarCipher testCaesarCipher = new CaesarCipher("ab", "cd");
         assertEquals(true,testCaesarCipher instanceof CaesarCipher);
+    }
+
+    @Test
+    public void newCaesarCipher_getsInput_String() {
+        CaesarCipher testCaesaeCipher = new CaesarCipher("ab","cd");
+        assertEquals("ab",testCaesaeCipher.getInput());
+
+
+    }
+
+    @Test
+    public void getOutPut_getsCaesarCipherOutput_String() {
+        CaesarCipher
     }
 }
