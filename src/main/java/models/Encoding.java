9140c01845d41d4f9ecfc85e14fccb4aea41341e
KevinMokoro <klosvoke1@gmail.com>
2021-04-12 15:46:58 +0300

    Saved the constructor's parameters to private member variables and added their getter methods.

--- src/main/java/models/Encoding.java
+++ src/main/java/models/Encoding.java
@@ -1,8 +1,20 @@
 package models;
 
 public class Encoding{
+    private String userInput;
+    private String userOutput;
 
     public Encoding(String userInput,String userOutput,int key) {
+        this.userInput = userInput;
+        this.userOutput = userOutput;
 
+    }
+
+    public String getUserInput() {
+        return this.userInput;
+    }
+
+    public String getUseOutput() {
+        return this.userOutput;
     }
 }
--- src/test/java/models/EncodingTest.java
+++ src/test/java/models/EncodingTest.java
@@ -10,4 +10,16 @@ public class EncodingTest {
         Encoding testEncoding = new Encoding("ab","cd",2);
         assertEquals(true,testEncoding instanceof Encoding);
     }
+
+    @Test
+    public void newEncoding_getsUserInput_String() {
+        Encoding testEncoding = new Encoding("abb","cdd",2);
+        assertEquals("abb",testEncoding.getUserInput());
+    }
+
+    @Test
+    public void getUserOutput_getsEncodedOutput_String() {
+        Encoding testEncoding = new Encoding("abb", "cdd",2);
+        assertEquals("cdd",testEncoding.getUseOutput());
+    }
 }
